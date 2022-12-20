af824509e6ff94ef193527cc9adabb17964b1600
Michael Howitz <mh@gocept.com>
2022-12-20 11:46:09 +0100

    Bumped version for breaking release.

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ tests_require = [
 ]
 
 setup(name='zope.app.catalog',
-      version='4.1.0.dev0',
+      version='5.0.dev0',
       author='Zope Corporation and Contributors',
       author_email='zope-dev@zope.org',
       description='Management pages for Zope Catalog',
